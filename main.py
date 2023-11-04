6bd012ea0b10039e0256a8bac83f595e2a031654
kael8 <72660817+kael8@users.noreply.github.com>
2023-11-04 22:03:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-from flask import Flask, request, jsonify
-import pickle
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.text import Tokenizer
+from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
+
+
 @app.route('/')
 def index():
-    return jsonify({"wregwsfnj": "rfhirtm"})
+    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
