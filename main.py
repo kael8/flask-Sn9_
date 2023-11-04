a25cd73d05afb07871f0e747f5fd44246a930f98
kael8 <72660817+kael8@users.noreply.github.com>
2023-11-05 02:07:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,11 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/sample_response', methods=['GET'])
-def sample_response():
-    mess = request.args.get('id', default=None)
-    
-    if mess is not None:
-        response_data = {'message': 'This is a sample response 2', 'status': mess}
-        status_code = 200  # 200 OK
-    else:
-        response_data = {'message': 'No "id" parameter provided', 'status': 'error'}
-        status_code = 404  # 404 Not Found
-    
-    return jsonify(response_data), status_code
+@app.route('/')
+def index():
+    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=int(os.getenv("PORT", 5000))
