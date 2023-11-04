a348cd96d2008d36470824a2f239617f7cc48b3c
kael8 <72660817+kael8@users.noreply.github.com>
2023-11-05 00:23:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from flask import Flask, request, jsonify
 import os
 
-app = Flask(__name__)
+app = Flask(__name)
 
-
-@app.route('/predict_sentiment', methods=['GET'])
+@app.route('/sample_response', methods=['GET'])
 def sample_response():
-    mess = request.id
-    response_data = {'message': 'This is a sample response 2', 'status': mess}
-    return jsonify(response_data)
-
+    mess = request.args.get('id', default=None)
+    
+    if mess is not None:
+        response_data = {'message': 'This is a sample response 2', 'status': mess}
+        status_code = 200  # 200 OK
+    else:
+        response_data = {'message': 'No "id" parameter provided', 'status': 'error'}
+        status_code = 404  # 404 Not Found
+    
+    return jsonify(response_data), status_code
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=int(os.getenv("PORT", 5000))
