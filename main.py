99f22af70358053b791000e822f2e06a8f0b8855
kael8 <72660817+kael8@users.noreply.github.com>
2023-11-05 02:35:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from flask import Flask, request, jsonify
-import os
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/sample_response')
+@app.route('/sample_response', methods=['GET'])
 def sample_response():
     response_data = {'message': 'This is a sample response', 'status': 'OK'}
     return jsonify(response_data)
 
-
-
 if __name__ == '__main__':
-    app.run(debug=True, port=int(os.getenv("PORT", 5000))
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
