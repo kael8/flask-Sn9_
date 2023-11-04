f7c0efb27efea6c91bad13d3ed18be294bf6596d
kael8 <72660817+kael8@users.noreply.github.com>
2023-11-04 22:14:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,45 +10,11 @@ import os
 app = Flask(__name__)
 
 
-# Load and merge the partitioned tokenizer states
-tokenizer_state_filenames = [
-    'model/tokenizer_state_part0.pkl',
-    'model/tokenizer_state_part1.pkl'
-]
-
-# Initialize an empty tokenizer
-tokenizer = Tokenizer()
-tokenizer.word_index = {}
-tokenizer.index_word = {}
-tokenizer.word_counts = {}
-tokenizer.document_count = 0
-
-# Iterate through each part, load it, and merge it into the complete tokenizer
-for part_filename in tokenizer_state_filenames:
-    with open(part_filename, 'rb') as handle:
-        part_tokenizer_state = pickle.load(handle)
-        
-    part_tokenizer = Tokenizer()
-    part_tokenizer.__dict__.update(part_tokenizer_state)
-    
-    # Merge the part tokenizer into the complete tokenizer
-    tokenizer.word_index.update(part_tokenizer.word_index)
-    tokenizer.word_counts.update(part_tokenizer.word_counts)
-    tokenizer.document_count += part_tokenizer.document_count
-
-# Set the num_words attribute based on your previous information
-tokenizer.num_words = 10000
-
-# Load the trained model
-model_filename = 'model/sentiment_analysis_model.h5'
-model = load_model(model_filename)
-
-# API endpoint to predict sentiment
 @app.route('/predict_sentiment', methods=['POST'])
 def predict_sentiment():
     data = request.get_json(force=True)
     text = data['text']
-
+    return jsonify({"dfg":"sgsed"})
     # Convert new texts to sequences using the loaded tokenizer
     new_sequences = tokenizer.texts_to_sequences([text])
 
