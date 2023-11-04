7c271d56bed0e7b5e4c72d610fa396349fd1ffce
kael8 <72660817+kael8@users.noreply.github.com>
2023-11-04 22:38:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,33 +12,7 @@ app = Flask(__name__)
 
 @app.route('/predict_sentiment', methods=['POST'])
 def predict_sentiment():
-    try:
-        data = request.get_json(force=True)
-        text = data['text']
-
-        # Convert new texts to sequences using the loaded tokenizer
-        new_sequences = tokenizer.texts_to_sequences([text])
-
-        # Map out-of-vocabulary words to a special token
-        for i, seq in enumerate(new_sequences):
-            new_sequences[i] = [token if 1 <= token <= tokenizer.num_words else 1 for token in seq]
-
-        max_sequence_length = 100
-
-        # Pad sequences to have a consistent length
-        new_X = pad_sequences(new_sequences, maxlen=max_sequence_length)
-
-        # Predict using the loaded model
-        predictions = model.predict(new_X)
-
-        # Determine sentiment based on the prediction
-        sentiment = "Positive" if predictions[0][0] >= 0.5 else "Negative"
-
-        return jsonify({'text': text, 'predicted_sentiment': sentiment, 'prediction_score': float(predictions[0][0]})
-
-    except Exception as e:
-        error_message = str(e)
-        return jsonify({'error': error_message}), 500  # 500 is the HTTP status code for Internal Server Error
+    return jsonify({'error': 'esrtt'}), 500  # 500 is the HTTP status code for Internal Server Error
 
 
 if __name__ == '__main__':
