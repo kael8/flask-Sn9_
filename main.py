46882ddbf25ff8dfe112e7948a025fb37e941278
kael8 <72660817+kael8@users.noreply.github.com>
2023-11-05 14:48:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/sample_response', methods=['GET'])
-def sample_response():
-    response_data = {'message': 'This is a sample response', 'status': 'Modified message'}
-    return jsonify(response_data)
+@app.route('/predict_sentiment', methods=['POST'])
+def predict_sentiment():
+    text = request.json['text']
+    # Perform sentiment analysis here
+    sentiment = "Positive"  # This should be replaced with actual sentiment analysis result
+    return jsonify({'sentiment': sentiment})
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
