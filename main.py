24c647b3f82bfb23f33077940c7e01466c4b3974
kael8 <72660817+kael8@users.noreply.github.com>
2023-11-13 21:04:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/predict_sentiment', methods=['POST'])
 def index():
-    return jsonify({'text': 'Working!!!'})
+    # Get the 'text' from the POST request
+    data = request.json
+    text_from_request = data.get('text', 'No text provided')
+
+    # Use the received text in the response
+    response_data = {'text': f'Received: {text_from_request}'}
+
+    return jsonify(response_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
